fb43bacc80cda5f90aa776769d9eb55b5882ed71
eitandwane27 <yuukimichi27@gmail.com>
2025-10-02 20:12:35 +0800

    modify design

--- revalue/users.sql
+++ revalue/users.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 01, 2025 at 05:37 AM
+-- Generation Time: Oct 02, 2025 at 09:51 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,106 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) DEFAULT 1,
+  `added_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `product_id`, `quantity`, `added_at`) VALUES
+(16, 1, 4, 1, '2025-10-02 07:40:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `size` varchar(3) NOT NULL,
+  `price` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `name`, `image`, `category`, `size`, `price`) VALUES
+(1, 'tshirt ni brual', 'uploads/1759377722_553683751_1270661271480494_3361838923040416034_n.jpg', 'vintage', 'M', 5000),
+(4, 'sando ni osit', 'uploads/1759378077_Screenshot 2025-08-27 221341.png', 'modern', 'XXL', 123123),
+(5, 'sumbrelo ni marvin', 'uploads/1759381740_Screenshot 2025-08-27 221704.png', 'vintage', 'M', 120000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `total_amount` decimal(10,2) NOT NULL,
+  `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(50) NOT NULL DEFAULT 'Pending',
+  `payment_method` varchar(50) NOT NULL DEFAULT 'Cash on Delivery'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `total_amount`, `order_date`, `status`, `payment_method`) VALUES
+(1, 1, 248123.00, '2025-10-02 07:12:06', 'Pending', 'Cash on Delivery'),
+(2, 1, 248123.00, '2025-10-02 07:13:13', 'Pending', 'Cash on Delivery'),
+(3, 1, 120000.00, '2025-10-02 07:19:54', 'Pending', 'Cash on Delivery'),
+(4, 1, 123123.00, '2025-10-02 07:22:23', 'Pending', 'Cash on Delivery'),
+(5, 1, 120000.00, '2025-10-02 07:41:04', 'Pending', 'Cash on Delivery');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`id`, `order_id`, `product_id`, `quantity`, `price`) VALUES
+(1, 1, 1, 1, 5000.00),
+(2, 1, 4, 1, 123123.00),
+(3, 1, 5, 1, 120000.00),
+(4, 2, 1, 1, 5000.00),
+(5, 2, 4, 1, 123123.00),
+(6, 2, 5, 1, 120000.00),
+(7, 3, 5, 1, 0.00),
+(8, 4, 4, 1, 0.00),
+(9, 5, 5, 1, 0.00);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -34,7 +134,7 @@ CREATE TABLE `users` (
   `Pass` varchar(255) NOT NULL,
   `F_name` varchar(255) NOT NULL,
   `L_name` varchar(255) NOT NULL,
-  `number` int(12) NOT NULL,
+  `number` varchar(16) NOT NULL,
   `birth_date` date DEFAULT NULL,
   `address` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL
@@ -45,16 +145,41 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `Full_name`, `E_mail`, `Pass`, `F_name`, `L_name`, `number`, `birth_date`, `address`, `country`) VALUES
-(1, 'david heard', 'heard_David@rocketmail.co', '$2y$10$1Vbuz3X5tatkxEMcCrjpdu/dDEXEAz5C/EbSqP6qbsHz1y8bhQnKy', '', '', 0, NULL, '', ''),
-(2, 'marvin pogi', 'angpogiko@gmail.com', '$2y$10$DcVC3Qsi2rtunraENTlMFOV9FhWdk7xQeoTzCX55.ja3BuFkKfDTG', '', '', 0, NULL, '', ''),
-(7, 'eitan maceda', 'tantan@gmail.com', '$2y$10$7fv1gi8rgT5XFbNwNe.HBu24MYVZXfixlNQYso/LRdPX0G9VMOOk.', '', '', 0, NULL, '', ''),
-(17, 'david heard', 'heard@gmail.com', '$2y$10$uDjBHR66LHYVTlY2BGbrnegko8LWrBv9ufdrH7zOtIBtGgdIzE3cy', '', '', 0, NULL, '', ''),
-(18, 'admin', 'admin@revalue.com', '$2y$10$CBUYIhNFR/qWt6gWa5wYQ.eRjLITzyQ3rqclKv/ZoZ79GnvltcgZ6', '', '', 0, NULL, '', ''),
-(19, 'david pogi', 'david@gmail.com', '$2y$10$gS0mK9ArmiL3uZ3Cag6MTep0OJhlwSCrN6QRvD.ohhEybDfj7TG5m', '', '', 0, NULL, '', '');
+(1, 'David Heard', 'heard_David@rocketmail.co', '$2y$10$1Vbuz3X5tatkxEMcCrjpdu/dDEXEAz5C/EbSqP6qbsHz1y8bhQnKy', 'David', 'Heard', '09760492077', '1999-08-16', 'Landayan', 'PH'),
+(18, 'admin', 'admin@revalue.com', '$2y$10$CBUYIhNFR/qWt6gWa5wYQ.eRjLITzyQ3rqclKv/ZoZ79GnvltcgZ6', '', '', '0', NULL, '', '');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `users`
@@ -68,10 +193,58 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `inventory` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `inventory` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
